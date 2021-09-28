ca762eec931d77835a049434704a7c7a21d8d182
MartinSomsak00 <xsomsakm@seznam.cz>
2021-09-28 13:25:25 +0200

    1

---
+++ Labs/01-tools/README.md
@@ -0,0 +1,20 @@
+# Lab 1
+Link to [Assignment]()
+### Blink example
+
+1. What is the meaning of the following binary operators in C?
+   * `|` OR
+   * `&` AND
+   * `^` NOR 
+   * `~` NOT
+   * `<<` left bitwise shift
+   * `>>` right bitwise shift
+
+2. Complete truth table with operators: `|`, `&`, `^`, `~`
+
+| **b** | **a** |**b or a** | **b and a** | **b xor a** | **not b** |
+| :-: | :-: | :-: | :-: | :-: | :-: |
+| 0 | 0 | 0 | 0 | 0 | 1 |
+| 0 | 1 | 1 | 0 | 1 | 0 |
+| 1 | 0 | 1 | 0 | 1 | 1 |
+| 1 | 1 | 1 | 1 | 0 | 0 |
--- Labs/01-tools/led/led/main.c
+++ Labs/01-tools/led/led/main.c
@@ -1,3 +1,4 @@
+
 /***********************************************************************
  * 
  * Blink a LED and use function from the delay library.
@@ -20,7 +21,12 @@
  * directives. This is a common mistake.
  */
 #define LED_GREEN   PB5 // AVR pin where green LED is connected
-#define SHORT_DELAY 250 // Delay in milliseconds
+
+#define DOT_DELAY   200 // Delay in milliseconds, PARIS
+#define DASH_DELAY  3*DOT_DELAY
+#define INTER_DELAY 1*DOT_DELAY
+#define CHAR_DELAY  DASH_DELAY-INTER_DELAY
+#define WORD_DELAY  7*DOT_DELAY-INTER_DELAY
 #ifndef F_CPU           // Preprocessor directive allows for conditional
                         // compilation. The #ifndef means "if not defined".
 # define F_CPU 16000000 // CPU frequency in Hz required for delay
@@ -35,8 +41,32 @@
 
 /* Function definitions ----------------------------------------------*/
 /**********************************************************************
+ * Function: dot function for displaying dot in Morse code on LED
+ * Returns:  none
+ **********************************************************************/
+void dot()
+{
+    // Invert LED in Data Register
+    // PORTB = PORTB xor 0010 0000
+    PORTB = PORTB ^ (1<<LED_GREEN);
+    _delay_ms(DOT_DELAY);
+    PORTB = PORTB ^ (1<<LED_GREEN);
+    _delay_ms(INTER_DELAY);
+}
+/**********************************************************************
+ * Function: dash function for displaying dash in Morse code on LED
+ * Returns:  none
+ **********************************************************************/
+void dash()
+{
+    PORTB = PORTB ^ (1<<LED_GREEN);
+    _delay_ms(DASH_DELAY);
+    PORTB = PORTB ^ (1<<LED_GREEN);
+    _delay_ms(INTER_DELAY);
+}
+/**********************************************************************
  * Function: Main function where the program execution begins
- * Purpose:  Toggle one LED and use delay library.
+ * Purpose:  Display on LED in Morse code "DE2"
  * Returns:  none
  **********************************************************************/
 int main(void)
@@ -52,15 +82,21 @@ int main(void)
     // Infinite loop
     while (1)
     {
-        // Pause several milliseconds
-        _delay_ms(SHORT_DELAY);
-
-        // Invert LED in Data Register
-        // PORTB = PORTB xor 0010 0000
-        PORTB = PORTB ^ (1<<LED_GREEN);
+        // "DE2" in Morse code "-.. . ..---"
+        dash();
+        dot();
+        dot();
+        _delay_ms(CHAR_DELAY);
+        dot();
+        _delay_ms(CHAR_DELAY);
+        dot();
+        dot();
+        dash();
+        dash();
+        dash();
+        _delay_ms(WORD_DELAY);
     }
 
     // Will never reach this
     return 0;
 }
-
